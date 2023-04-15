34209f0bb61c91632df848500b0cb1a85b0fd19c
VictorGV <victor.vides@cencosud.com.ar>
2023-04-15 15:11:12 -0300

    Guia 1 a 5

---
+++ GuiaEjercicios_Clase1/Guia de ejercicios Clase 1.js
@@ -0,0 +1,29 @@
+// 1 
+let auto = "Corolla"
+console.log(auto)
+console.log(typeof(auto))
+
+
+// 2
+auto = "308"
+console.log(auto)
+console.log(typeof(auto))
+
+
+// 3
+auto = "C4"
+console.log(auto)
+console.log(typeof(auto))
+
+
+// 4
+let nombre = "Gabriel"
+let apellido = "Vides"
+console.log(nombre + ' ' + apellido)
+
+// 5
+let fruta = "naranja"
+let tipoDeLaVariableFruta = typeof(fruta)
+console.log("Tipo de la variable fruta: "+tipoDeLaVariableFruta)
+
+
